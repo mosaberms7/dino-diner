631df4188baf41194aeb9f81e5cd867e3d68c6f1
Mohamed Fares <mosaber7@ksu.edu>
2019-09-27 22:19:50 +0200

    09/27

--- MenuTest/Drinks/WaterTest.cs
+++ MenuTest/Drinks/WaterTest.cs
@@ -1,10 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DinoDiner.Menu.Drinks;
+using Xunit;
 
 namespace MenuTest.Drinks
 {
     class WaterTest
     {
+        public void ShouldHaveCorrectDefaultPrice()
+        {
+            Water w = new Water();
+            Assert.Equal(0.11, w.Price);
+        }
+        public void ShouldHaveTheDefaultCalories()
+        {
+            Water w = new Water();
+            Assert.Equal<uint>(0,w.Calories);
+        }
+
+        public void ShouldHaveTheDefaultSize()
+        {
+            Water w=new Water();
+            Assert.Equal(Size.Small, w.Size);
+        }
+        public void ShouldHaveDefaultIce()
+        {
+            Water w = new Water();
+            Assert.True(w.Ice);
+        }
+
+        public void ShouldHaveDefaultLemon()
+        {
+            Water w = new Water();
+            Assert.False(w.Lemon);
+
+        }
     }
 }
